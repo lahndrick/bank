8c543a50b4e34ddecae581a86c0d66fac1ec385a
Lahndrick <61927673+lahndrick@users.noreply.github.com>
2022-09-16 12:45:32 +1200

    Create Bank2.java

---
+++ Bank2.java
@@ -0,0 +1,94 @@
+import java.util.ArrayList;
+
+public class Bank
+{
+	private int accountNumber = 100;
+	private ArrayList<Customer> customers;
+
+	public Bank()
+	{
+		customers = new ArrayList<Customer>();
+	}
+
+	public void addCustomer(Customer customer)
+	{
+		customer.setID(accountNumber);
+		accountNumber++;
+		this.customers.add(customer);
+	}
+
+	public void removeCustomer(Customer customer)
+	{
+		this.customers.remove(customer);
+	}
+
+	public String toString()
+	{
+		for(int x = 0; x < customers.size();x++)
+		{
+			System.out.println(customers.get(x));
+		}
+		return "";
+	}
+
+	public Customer getCustomer(int x)
+	{
+		return customers.get(x);
+	}
+
+	public Customer max()
+	{
+		Customer maxCustomer = null;
+		double max = 0;
+
+		for(int x = 0;x < customers.size();x++)
+		{
+			if(customers.get(x).getBalance() > max)
+			{
+				max = customers.get(x).getBalance();
+				maxCustomer = customers.get(x);
+			}
+		}
+
+		return maxCustomer;
+	}
+
+	public Customer min()
+	{
+		Customer minCustomer = null;
+		double min = this.average();
+
+		for(int x = 0;x < customers.size();x++)
+		{
+			if(customers.get(x).getBalance() <=  min)
+			{
+				min = customers.get(x).getBalance();
+				minCustomer = customers.get(x);
+			}
+		}
+
+		return minCustomer;
+	}
+
+	public int totalAmount()
+	{
+		int total = 0;
+
+		for(int x = 0; x < customers.size();x++)
+		{
+			total += customers.get(x).getBalance();
+		}
+
+		return total;
+	}
+
+	public int average()
+	{
+		return this.totalAmount() / customers.size();
+	}
+
+	public int size()
+	{
+		return this.customers.size();
+	}
+}
